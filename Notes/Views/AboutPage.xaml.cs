1492f8bdcaa36bf1c1573013acc4975a67bf2beb
Juan Ignacio Avendaño Huergo <juan.avendano@concatel.com>
2023-06-30 13:10:07 +0200

    Add project files.

---
+++ Notes/Views/AboutPage.xaml.cs
@@ -0,0 +1,26 @@
+namespace Notes.Views;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+        //BindingContext = new Models.About();
+	}
+
+    private async void LearnMore_Clicked(object sender, EventArgs e)
+    {
+        /*Models.About about;
+        if (BindingContext.GetType().Equals(typeof(Models.About)))
+        {
+            about = (Models.About)BindingContext;
+            await Launcher.Default.OpenAsync(about.MoreInfoUrl);
+        }*/
+
+        if (BindingContext is Models.About about)
+        {
+            // Navigate to the specified URL in the system browser.
+            await Launcher.Default.OpenAsync(about.MoreInfoUrl);
+        }
+    }
+}
