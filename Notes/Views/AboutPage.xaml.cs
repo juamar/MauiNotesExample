985cdf3d0514d5e22e208662e718c712ba6bea48
Juan Ignacio Avendaño Huergo <juan.avendano@concatel.com>
2023-07-13 16:14:52 +0200

    Done

--- Notes/Views/AboutPage.xaml.cs
+++ Notes/Views/AboutPage.xaml.cs
@@ -8,19 +8,19 @@ public partial class AboutPage : ContentPage
         //BindingContext = new Models.About();
 	}
 
-    private async void LearnMore_Clicked(object sender, EventArgs e)
+    /*private async void LearnMore_Clicked(object sender, EventArgs e)
     {
-        /*Models.About about;
-        if (BindingContext.GetType().Equals(typeof(Models.About)))
-        {
-            about = (Models.About)BindingContext;
-            await Launcher.Default.OpenAsync(about.MoreInfoUrl);
-        }*/
+        //Models.About about;
+        //if (BindingContext.GetType().Equals(typeof(Models.About)))
+        //{
+        //    about = (Models.About)BindingContext;
+        //    await Launcher.Default.OpenAsync(about.MoreInfoUrl);
+        //}
 
         if (BindingContext is Models.About about)
         {
             // Navigate to the specified URL in the system browser.
             await Launcher.Default.OpenAsync(about.MoreInfoUrl);
         }
-    }
+    }*/
 }
